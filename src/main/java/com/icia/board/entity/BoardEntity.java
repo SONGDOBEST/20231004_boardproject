4244b44469ae800e5c8e020ae4232057ed777002
songdobest <dlgusdn627@naver.com>
2023-10-04 14:05:46 +0900

    Initial commit

---
+++ src/main/java/com/icia/board/entity/BoardEntity.java
@@ -0,0 +1,54 @@
+package com.icia.board.entity;
+
+import com.icia.board.dto.BoardDTO;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter(AccessLevel.PRIVATE)
+@Entity
+@Table(name = "board_table")
+public class BoardEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String boardWriter;
+
+    @Column(nullable = false)
+    private String boardTitle;
+
+    @Column(nullable = false)
+    private String boardPass;
+
+    @Column
+    private String boardContents;
+
+    @Column
+    private int boardHits;
+
+    public static BoardEntity toSaveEntity(BoardDTO boardDTO){
+        BoardEntity boardEntity = new BoardEntity();
+        boardEntity.setBoardWriter(boardDTO.getBoardWriter());
+        boardEntity.setBoardTitle(boardDTO.getBoardTitle());
+        boardEntity.setBoardPass(boardDTO.getBoardPass());
+        boardEntity.setBoardContents(boardDTO.getBoardContents());
+        boardEntity.setBoardHits(boardDTO.getBoardHits());
+        return boardEntity;
+    }
+
+    public static BoardEntity toUpdateEntity(BoardDTO boardDTO){
+        BoardEntity boardEntity = new BoardEntity();
+        boardEntity.setId(boardDTO.getId());
+        boardEntity.setBoardWriter(boardDTO.getBoardWriter());
+        boardEntity.setBoardTitle(boardDTO.getBoardTitle());
+        boardEntity.setBoardPass(boardDTO.getBoardPass());
+        boardEntity.setBoardContents(boardDTO.getBoardContents());
+        boardEntity.setBoardHits(boardDTO.getBoardHits());
+        return boardEntity;
+    }
+}
